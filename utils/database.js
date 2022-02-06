7774203b00369ad91938ed56a425000c60063856
Vivek <vivekkumarsingh2002@gmail.com>
2022-02-06 07:09:36 +0530

    docker-fix

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
+require("dotenv").config();
+
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'reportsFeed',
-    password: 'Jaimatadi',
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 
